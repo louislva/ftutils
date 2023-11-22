342440e6250195061e9521c2832aa6b169321c50
Louis Arge <louislva@gmail.com>
2023-11-22 14:48:39 +0100

    dataset class

--- src/conversation.py
+++ src/conversation.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 from openai.types.chat import ChatCompletionMessage
 import re
 from typing import Literal, Optional
+import json
 client = OpenAI()
 
 class Message:
@@ -54,16 +55,29 @@ class Conversation:
         # Parse as Message objects
         messages = [Message.from_text(text[start:end]) for start, end in zip(split_indicies, split_indicies[1:] + [len(text)])]
         return Conversation(messages)
-    
     @staticmethod
     def from_file(path: str, extra_roles: list[str] = []):
         return Conversation.from_text(open(path).read(), extra_roles=extra_roles)
     
     def to_text(self) -> str:
         return "".join([msg.to_text() for msg in self.messages])
-    def to_file(self, path: str) -> None:
+    def to_file(self, path: str):
         open(path, "w").write(self.to_text())
 
+    def to_json(self):
+        return {"messages": [msg.to_json() for msg in self.messages]}
+
+class Dataset:
+    def __init__(self, conversations: list[Conversation] = []):
+        self.conversations = conversations
+    def append(self, conversation: Conversation): self.conversations.append(conversation)
+
+    def to_jsonl(self):
+        return "\n".join([json.dumps(conversation.to_json()) for conversation in self.conversations])
+    def to_file(self, path):
+        open(path, "w").write(self.to_jsonl())
+
 if __name__ == "__main__":
-    convo = Conversation.from_file("copy.txt")
-    convo.to_file("copy1.txt")
+    convo = Conversation.from_file("convo.txt")
+    dataset = Dataset([convo, convo, convo])
+    dataset.to_file("train.jsonl")
