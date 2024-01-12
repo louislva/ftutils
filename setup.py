4cc2bdfebeb7ad7716399d9a01324cd8899a85ee
Louis Arge <louislva@gmail.com>
2024-01-12 12:58:46 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ftutils',
+    version='0.1',
+    packages=find_packages(),
+    description='Fine-tuning utilities for conversational AI.',
+    author='Louis Arge',
+    author_email='louislva@gmail.com',
+    url='https://github.com/louislva/ftutils',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
