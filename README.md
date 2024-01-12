681636c447ff1ec7c20e7c58b13f0bb8c1123b7e
Louis Arge <louislva@gmail.com>
2024-01-12 13:01:29 -0600

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# ftutils
+
+Fine-tuning utilities for conversational language models.
+
+## Installation
+
+```shell
+pip install git+https://github.com/louislva/ftutils.git
+```
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ftutils',
     version='0.1',
     packages=find_packages(),
-    description='Fine-tuning utilities for conversational AI.',
+    description='Fine-tuning utilities for conversational language models.',
     author='Louis Arge',
     author_email='louislva@gmail.com',
     url='https://github.com/louislva/ftutils',
